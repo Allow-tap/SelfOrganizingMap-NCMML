c8204cf8b750d88e725395624c3db03220317a53
Jonathan Lindén <jonathan@jlinden.se>
2021-05-15 14:36:34 +0200

    Parallelized hyperparameter tuning and removed quantization error as a measure.

--- hyperparameter_tuning.py
+++ hyperparameter_tuning.py
@@ -1,4 +1,5 @@
 import ast
+import multiprocessing
 import pickle
 
 import matplotlib.pyplot as plt
@@ -15,7 +16,12 @@ from sklearn.model_selection import ParameterGrid
 som_neurons = (44, 44)
 epochs = 100
 learning_rates = [0.001, 0.01, 0.1]
-sigmas = [0.5, 1, 1.5]
+sigmas = [1.5, 2, 2.5, 3, 3.5]
+
+# Set this manually if you don't want to use all CPUs
+# max_n_cpus = 4  # like this
+max_n_cpus = multiprocessing.cpu_count()
+
 genres = ["rock",
           "classical",
           "latin",
@@ -52,23 +58,26 @@ parameter_combinations = list(ParameterGrid({
     'learning_rate': learning_rates,
     'sigma': sigmas}))
 
-print('Training models...')
-for i, params in enumerate(parameter_combinations):
+for i, x in enumerate(parameter_combinations):
+    parameter_combinations[i]['index'] = i+1
+
+
+def evaluate_model(params):
     learning_rate, sigma = params['learning_rate'], params['sigma']
-    print(f'Running model {i+1} of {len(parameter_combinations)} with '
-          f'learning rate={learning_rate} and sigma={sigma}')
+    index = params['index']
+
+    print(f'Running model {index} with learning rate={learning_rate} and '
+          f'sigma={sigma}')
     som = MiniSom(som_neurons[0], som_neurons[1], len(features),
                   sigma=sigma, learning_rate=learning_rate)
 
-    quantization_errors = []
     silhouette_scores = []
     davies_bouldin_scores = []
     calinski_harabasz_scores = []
 
-    for i in range(epochs):
-        print(f'Running epoch {i+1}/{epochs}')
-        som.train(tracks_values, tracks_values.shape[0], verbose=True)
-        quantization_errors.append(som.quantization_error(tracks_values))
+    for epoch in range(epochs):
+        print(f'Model {index} running epoch {epoch+1}/{epochs}')
+        som.train(tracks_values, tracks_values.shape[0], verbose=False)
 
         som_weights = som.get_weights()
         som_weights_flat = som_weights.reshape(som_neurons[0] * som_neurons[1],
@@ -78,28 +87,24 @@ for i, params in enumerate(parameter_combinations):
         davies_bouldin = davies_bouldin_score(som_weights_flat, labels)
         calinski_harabasz = calinski_harabasz_score(som_weights_flat, labels)
 
-        print(f'Silhouette score: {silhouette:.3f}')
-        print(f'Davies-Bouldin score: {davies_bouldin:.3f} (lower is better)')
-        print(f'Calinski-Harabasz score: {calinski_harabasz:.3f} '
-              f'(higher is better)\n')
-
         silhouette_scores.append(silhouette)
         davies_bouldin_scores.append(davies_bouldin)
         calinski_harabasz_scores.append(calinski_harabasz)
 
         fig, ax1 = plt.subplots()
-        ax1.plot(silhouette_scores, color='b', linestyle='-', label='Silhouette')
-        ax1.plot(davies_bouldin_scores, color='b', linestyle='-.',
-                 label='Davies-Bouldin')
-        ax1.plot(quantization_errors, color='b', linestyle='--',
-                 label='Quantization error')
+        x_range = range(1, len(silhouette_scores)+1)
+        ax1.plot(x_range, silhouette_scores, color='b',
+                 linestyle='-', label='Silhouette')
+        ax1.plot(x_range, davies_bouldin_scores, color='b',
+                 linestyle='-.', label='Davies-Bouldin')
         ax1.tick_params(axis='y', labelcolor='b')
         ax1.set_xlabel('Epochs')
 
         ax2 = plt.twinx()
-        ax2.plot(calinski_harabasz_scores, color='r', linestyle=':',
-                 label='Calinski-Harabasz')
+        ax2.plot(x_range, calinski_harabasz_scores, color='r',
+                 linestyle=':', label='Calinski-Harabasz')
         ax2.tick_params(axis='y', labelcolor='r')
+        ax2.set_ylim(0, 1000)
         ax1.legend()
         ax2.legend()
         fig.tight_layout()
@@ -108,7 +113,19 @@ for i, params in enumerate(parameter_combinations):
 
         plt.savefig(f'figures/{cluster_method_name}_training_'
                     f'lr_{learning_rate}_sigma_{sigma}.png')
+        plt.close()
 
         with open(f'models/{cluster_method_name}_training_'
                   f'lr_{learning_rate}_sigma_{sigma}.p', 'wb') as model_file:
             pickle.dump(som, model_file)
+
+        print(f'Model {index} done with epoch {epoch+1}.')
+
+    print(f'Model {index} with learning rate={learning_rate} and '
+          f'sigma={sigma} finished.')
+
+
+print('Training models...')
+pool = multiprocessing.Pool(min(multiprocessing.cpu_count(),
+                                len(parameter_combinations)))
+pool.map(evaluate_model, parameter_combinations)
